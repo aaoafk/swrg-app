10c6918e6190ea56c1e34e5f0e0fd4a60bd75c07
Steven Fernandez <stevenfernandez@Stevens-MacBook-Air.local>
2024-11-25 10:38:46 -0500

    Update smtp process to just connect direct to smtp.gmail.com

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -62,7 +62,7 @@ Rails.application.configure do
   config.action_mailer.smtp_settings = {
     user_name: Rails.application.credentials.dig(:smtp, :user_name),
     password: Rails.application.credentials.dig(:smtp, :password),
-    address: "email-smtp.us-east-2.amazonaws.com",
+    address: "smtp.gmail.com",
     port: 587,
     authentication: :plain
   }
